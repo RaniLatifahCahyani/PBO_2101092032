f48bfe96d7903aa5db4e438614bfb51d18de3df8
RaniLatifahCahyani <114121848+RaniLatifahCahyani@users.noreply.github.com>
2022-12-02 12:13:59 +0700

    Create latihan3.java

---
+++ src/rani300922/latihan3.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rani300922;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author lenovo
+ */
+public class latihan3 {
+    public static void main( String[] args ){
+        String name ="";
+        name = JOptionPane.showInputDialog("Please Enter Your Name");
+        
+        String msg = "Hello " + name + "!";
+        
+        JOptionPane.showMessageDialog(null, msg);
+    
+    
+    
+    }
+}
