8b0afe4f9c0798fbd8e63cd4bf97cfd9a35b25f3
RaniLatifahCahyani <114121848+RaniLatifahCahyani@users.noreply.github.com>
2022-12-02 12:13:02 +0700

    Create latihan2_a.java

---
+++ src/rani281022/latihan2_a.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rani281022;
+
+/**
+ *
+ * @author lenovo
+ */
+
+// LATIHAN 1.WHILE//
+
+public class latihan2_a {
+    public static void main(String[] args) {
+        int i = 0;
+        String days[] = {"Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu","Minggu"};
+        
+        while (i < 7){
+            System.out.println(days[i]);
+            i++;
+        }
+    }
+}
